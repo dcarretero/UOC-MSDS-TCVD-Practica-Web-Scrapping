c1515f09b8df131509e4706e02712f9808a10e98
Daniel <daniel.carretero@gmail.com>
2021-10-20 17:56:54 +0200

    v.0.1.3- Versión básica con clase Recipe y carga de uno de sus atributos

--- PruebaBasica1/main.py
+++ PruebaBasica1/main.py
@@ -1,10 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
+class Recipe:
+    name =''
+    difficulty=''
+    recipeCategory = ''
+    recipeCuisine = ''
+    description = ''
+    image = ''
+    videoContentUrl = ''
+    calories = ''
+    totalTime = ''
+    recipeYield=''
+    estimatedCost=''
+    ratingValue=''
+    reviewCount=''
+    dataPublished=''
+    categoryTags= []
+
+
+    def loadRecipeFromUrl(self,url):
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        # Se obtiene el titulo de la receta
+        div_recipe = soup.find('div', id="recipe")
+        self.name=div_recipe.article.section.header.h1.text.strip()
+
 if __name__ == '__main__':
     URL = "https://www.recetasderechupete.com/todas/recetas/carnes-aves/recetas-con-pollo/"
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
+    recipes = []
 
     tag_current_page=soup.find_all('span', class_ ='page-numbers current')
     tag_next_page=soup.find_all('a', class_= 'next page-numbers')
@@ -22,3 +48,8 @@ if __name__ == '__main__':
         tags_recipes = soup.find_all('a', class_ ='recipephoto')
         for recipe in tags_recipes:
             print("receta " + recipe['href'])
+            currentRecipe=Recipe()
+            currentRecipe.loadRecipeFromUrl(recipe['href'])
+            recipes.append(currentRecipe)
+    #Evidencia de que se ha guardado bien
+    print(recipes[0].name)
