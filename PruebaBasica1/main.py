41fd3e174814a297b35e4090f23fc55c306abc30
Ivan Tecles <ivanteclesgasso@gmail.com>
2021-10-23 00:14:01 +0200

    Update main.py
    Completados los campos. Guarda imágenes en local.

--- PruebaBasica1/main.py
+++ PruebaBasica1/main.py
@@ -1,13 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
+def load_requests(source_url):
+    r = requests.get(source_url, stream = True)
+    if r.status_code == 200:
+        aSplit = source_url.split('/')
+        ruta = "/Users/ivantecles/Desktop/webscraping/PruebaBasica1/Images/"+aSplit[len(aSplit)-1]
+        print(ruta)
+        output = open(ruta,"wb")
+        for chunk in r:
+            output.write(chunk)
+        output.close()
+
 class Recipe:
+    url = ''
     name = ''
-    autor = ''
+    author = ''
     difficulty = ''
-    recipeCategory = ''
-    recipeCuisine = ''
-    description = ''
+    ingredients = []
     image = ''
     videoContentUrl = ''
     calories = ''
@@ -16,24 +26,43 @@ class Recipe:
     estimatedCost = ''
     ratingValue = ''
     reviewCount = ''
-    dataPublished = ''
     categoryTags = []
 
+    
 
     def loadRecipeFromUrl(self,url):
         page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
         # Se obtiene el titulo de la receta
         div_recipe = soup.find('div', id="recipe")
-        self.name=div_recipe.article.section.header.h1.text.strip()
-        self.image=div_recipe.article.section.header.img.get('src')
+        ingredients = str(div_recipe.find(id="ingredients").find('ul').find_all('li'))
 
-        rdr_tags=soup.find_all('span', class_='rdr-tag')
-        self.difficulty=rdr_tags[0].text
-        self.totalTime=rdr_tags[1].text
-        self.recipeYield=rdr_tags[2].text
-        self.ratingValue=soup.find('span', 'rf_average').text
-        self.reviewCount=soup.find('span', 'rf_count').text
+        rdr_tags=div_recipe.find_all('span', class_='rdr-tag')
+        extrainfo=div_recipe.find(id="extrainfo").find('ul').find_all('li')
+
+        for index, value in enumerate(extrainfo):
+            if index == 0:
+                self.estimatedCost = value.text
+            if index == 1:
+                self.calories = value.text
+            if index == 2:
+                categories = value.text
+                self.categoryTags = categories.split(" · ")
+
+        self.url = url
+        self.name = div_recipe.article.section.header.h1.text.strip()
+        self.author = div_recipe.find('p', class_ ='rdr-author').find('a').text
+        self.difficulty = rdr_tags[0].text
+        self.ingredients = ingredients
+        self.image = div_recipe.find('img', class_ = 'mainphoto').get('src')
+
+        load_requests(self.image)
+
+        self.videoContentUrl = str(div_recipe.find('div', class_ = "wp-block-embed__wrapper").find('iframe').attrs['src'])
+        self.totalTime = rdr_tags[1].text
+        self.recipeYield = rdr_tags[2].text
+        self.ratingValue = div_recipe.find('span', class_ = 'rf_average').text
+        self.reviewCount = div_recipe.find('span', class_ = 'rf_count').text
 
 if __name__ == '__main__':
     URL = "https://www.recetasderechupete.com/todas/recetas/carnes-aves/recetas-con-pollo/"
@@ -41,8 +70,8 @@ if __name__ == '__main__':
     soup = BeautifulSoup(page.content, "html.parser")
     recipes = []
 
-    tag_current_page=soup.find_all('span', class_ ='page-numbers current')
-    tag_next_page=soup.find_all('a', class_= 'next page-numbers')
+    tag_current_page=soup.find_all('span', class_ = 'page-numbers current')
+    tag_next_page=soup.find_all('a', class_ = 'next page-numbers')
     print(URL)
     print("numero pagina actual"+ tag_current_page[0].text.strip())
     while len(tag_next_page)>0 :
@@ -62,10 +91,18 @@ if __name__ == '__main__':
             recipes.append(currentRecipe)
             break
         break
-    #Evidencia de que se ha guardado bien
+    # Evidencia de que se ha guardado bien
+    print("receta url: " + recipes[0].url)
     print("receta name: " + recipes[0].name)
-    print("receta image: " + recipes[0].image)
+    print("receta author: " + recipes[0].author)
     print("receta difficulty: " + recipes[0].difficulty)
+    print("receta ingredients: " + str(recipes[0].ingredients))
+    print("receta videoContentUrl: " + recipes[0].videoContentUrl)
+    print("receta calories: " + recipes[0].calories)
     print("receta totalTime: " + recipes[0].totalTime)
     print("receta recipeYield: " + recipes[0].recipeYield)
+    print("receta estimatedCost: " + recipes[0].estimatedCost)
     print("receta ratingValue: " + recipes[0].ratingValue)
+    print("receta reviewCount: " + recipes[0].reviewCount)
+    print("receta categoryTags: " + str(recipes[0].categoryTags))
+    print("receta image: " + recipes[0].image)
